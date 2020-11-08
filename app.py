b837aab3c3f42ee91602463e8755fbbf23cf5599
Shrut Rakeshkumar Shah <68378230+Sshrut@users.noreply.github.com>
2020-11-08 12:14:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask,render_template,request
+import pickle
+import numpy as np
+from bs4 import BeautifulSoup
+import requests,html5lib,lxml
+from urllib.request import urlopen
+import re
+
+
+
+app=Flask(__name__)
+
+def ans_result(user_str):
+	splitstr=user_str.split(' ')
+	user_input='+'.join(splitstr)
+	search=requests.get("https://www.google.com/search?q=geeks+for+geeks+introduction+to+"+user_input).text
+	soup=BeautifulSoup(search,'html.parser')
+
+	new=soup.find('div',{'id':'main'})
+	asdf=new.find('div',{'class':'kCrYT'})
+	#print(new.prettify())
+	need=str(asdf.a)
+	if need=='None':
+		new=soup.find('div',{'class':'BNeawe UPmit AP7Wnd'}).text
+		l=new.split(' ')
+		final_url='https://'+l[0]+'/'+l[2]
+    
+	else:
+		ans=re.search("(?P<url>https?://[^\s]+)", need).group("url")
+		final_url=ans.split(';')[0][:-5]
+    
+    	#print(final_url)
+	page=requests.get(final_url).text
+
+	new_soup=BeautifulSoup(page,'html.parser')
+	content=new_soup.find('div',{'class':'entry-content'})
+	try:
+		allresult=content.find_all('p')
+		result=content.find('p').text
+	except:
+		pass
+
+	ans_f=[]
+	if result=='':
+		flag=0
+		for i in allresult:
+			ans.append(i.text)
+			flag+=1
+			if flag==2:
+				break
+		final_ans=''.join(ans_f)
+	else:
+		final_ans=result
+
+	return final_ans
+
+@app.route('/')
+def home():
+	
+	return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+	int_feature=request.form['key']
+	user_str=str(int_feature)
+	final_result=str(ans_result(user_str))
+	
+
+	return render_template('predict.html',answer=final_result)
+
+if __name__=='__main__':
+	app.run(debug=True)  
